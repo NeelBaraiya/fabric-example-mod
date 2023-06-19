a00436fce967d22a46a56863b2902181ae579e03
NeelBaraiya <nbaraiya159@student.fuhsd.org>
2023-06-18 18:27:56 -0700

    Revert "setting up workspace"
    This reverts commit a04cd4db30a8b7f70d5cd3dd404a138080958871.

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,10 +1,10 @@
-package net.neel.moreTridentEnchantments;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MoreTridentEnchantments implements ModInitializer {
+public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,7 +1,7 @@
-package net.neel.moreTridentEnchantments.mixin;
+package net.fabricmc.example.mixin;
 
+import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.neel.moreTridentEnchantments.MoreTridentEnchantments;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		MoreTridentEnchantments.LOGGER.info("This line is printed by an example mod mixin!");
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
